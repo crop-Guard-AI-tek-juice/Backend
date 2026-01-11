4a49203db3a1d73094b6ca6292fe96b795bb85d5
ayebaishmo <ishmo255super@gmail.com>
2026-01-11 12:37:46 +0300

    Nlp con

--- cassava_backend/api/services/api_service.py
+++ cassava_backend/api/services/api_service.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 from django.conf import settings
 
-genai.configure(api_key=settings.GEMINI_API_KEY)
+genai.configure(api_key=settings.NLP_API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
