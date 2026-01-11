93087a6fc71072aee45e9f912133e9b1ed16dfa0
ayebaishmo <ishmo255super@gmail.com>
2026-01-11 14:36:03 +0300

    advice remove

--- cassava_backend/api/urls.py
+++ cassava_backend/api/urls.py
@@ -4,5 +4,4 @@ from .views import PredictLeafDisease, predict_form_view, disease_advice
 urlpatterns = [
     path('predict/', PredictLeafDisease.as_view(), name='predict-leaf-disease'),
     path('predict-form/', predict_form_view, name='predict-form'),
-    path("advice/", disease_advice),
 ]
